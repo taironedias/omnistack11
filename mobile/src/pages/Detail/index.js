2efe9e5bd7f3927388c5184d13a411fe1f1fa3af
taironecdias <taironecdias@gmail.com>
2020-03-26 22:50:49 -0300

    mobile application

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Feather, FontAwesome5 } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+import styles from './styles';
+
+export default function Detail() {
+
+    const navigattion = useNavigation();
+    const route = useRoute();
+
+    const incident = route.params.incident;
+    const valueFormated = Intl.NumberFormat('pt-BR', { 
+        style: 'currency', 
+        currency: 'BRL'
+    }).format(incident.value);
+    const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${valueFormated}.`;
+
+    function navigateToIncidents() {
+        navigattion.goBack();
+    }
+
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: `${incident.title}`,
+            recipients: [incident.email],
+            body: message
+        })
+    }
+
+    function sendWhatsApp() {
+        let whatsapp = incident.whatsapp;
+        if (!whatsapp.startsWith(55)) {
+            whatsapp = '55'+incident.whatsapp;
+        }
+        Linking.openURL(`whatsapp://send?phone=${whatsapp}&text=${message}`);
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+                <TouchableOpacity style={styles.headerButton} onPress={navigateToIncidents}>
+                    <Feather name="arrow-left" size={28} color="#E82041" />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.incident}>
+                <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+                <Text style={styles.incidentValue}> {incident.name} de {incident.city}-{incident.uf} </Text>
+                
+                <Text style={styles.incidentProperty}>CASO:</Text>
+                <Text style={styles.incidentValue}> {incident.title}: {incident.description} </Text>
+
+                <Text style={styles.incidentProperty}>VALOR:</Text>
+                <Text style={styles.incidentValue}> {valueFormated} </Text>
+            </View>
+
+            <View style={styles.contactBox}>
+                <Text style={styles.heroTitle}>Salve o dia!</Text>
+                <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+                <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
+                        <FontAwesome5 name="whatsapp" size={15} color="#FFF" />
+                        <Text style={styles.actionText}>WhatsApp</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
+                        <FontAwesome5 name="envelope" size={15} color="#FFF" />
+                        <Text style={styles.actionText}>E-mail</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+}
