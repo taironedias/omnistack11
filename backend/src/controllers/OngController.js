415c36effcccbc31ced53090f07995b2e8132bdf
taironecdias <taironecdias@gmail.com>
2020-03-28 00:10:05 -0300

    desaclopando o generateUniqueId para realizar testes unitários

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,10 +1,10 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
